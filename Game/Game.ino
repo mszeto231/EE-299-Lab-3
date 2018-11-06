88b47538390994fa0ad65469f8f8eeceed3f80ff
Mitchell Szeto <mszeto@allumia.com>
2018-11-06 14:05:05 -0800

    bomb integration

--- Game/Game.ino
+++ Game/Game.ino
@@ -1,25 +1,28 @@
-#define SIZE 4
+#define SIZE 4          // size of board
+#define MAX_BOMBS 4     // max number of bombs allowed
 
-int matrix[4][4];
+int matrix[SIZE][SIZE];  // board
 bool nextMove = false;
+
 void setup() {
  Serial.begin(9600); 
- createMap(matrix);
+ randomSeed(analogRead(0));
+ createMap();
+ setRandomBombs();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
     int c = Serial.read();
     nextMove = userEnterMove(c);
   }
   if (nextMove) {
-    printMap(matrix);
+    printMap();
     nextMove = false;
   }
 }
 
-void createMap(int matrix[][SIZE]) {
+void createMap() {
   for (int i = 0; i < SIZE; i++) {
     for (int j = 0; j < SIZE; j++) {
       matrix[i][j] = 0;
@@ -27,7 +30,7 @@ void createMap(int matrix[][SIZE]) {
   }
 }
 
-void printMap(int matrix[][SIZE]) {
+void printMap() {
   for (int i = 0; i < SIZE; i++) {
     for (int j = 0; j < SIZE; j++) {
       if (matrix[i][j] == 0) {
@@ -49,4 +52,33 @@ bool userEnterMove(int c) {
   } else {
     return false;
   }
+}
+
+// randomly sets MAX_BOMBS on the board
+void setRandomBombs() {
+  int numBombs = 0;
+  while (numBombs < MAX_BOMBS) {
+    if (setBombs(random(0, SIZE), random(0, SIZE))){
+      numBombs++;
+    }
+  }
+}
+
+// row: row of board
+// col: column of board
+// tries to set the given position as a bomb
+// returns true if successful and false if not successful
+bool setBombs(int row, int col) {
+  if (matrix[row][col] == 1 || isCorner(row, col)) {
+    return false;
+  } else {
+    matrix[row][col] = 1;
+    return true;
+  }
+}
+
+// returns true if row and col make a corner
+// otherwise, returns false
+bool isCorner(int row, int col) {
+  return (row == 0 && col == 0) || (row == 0 && col == SIZE - 1) || (row == SIZE - 1 && col == 0) || (row == SIZE -1 && col == SIZE - 1);
 }
